c82425ba45209aeadc0a6b0c37ba2f6e863e135c
rafaeldatarock <rafaeldatarock@gmail.com>
2021-12-01 10:55:31 +0100

    Added phue module

--- main.py
+++ main.py
@@ -1,11 +1,51 @@
 from pynput import keyboard
+from phue import Bridge
+
+b = Bridge('')
+lights = b.lights
+api = b.get_api()
 
 def press_callback(key):
-    print('{} was pressed'.format(key))
+    if key == keyboard.Key.esc:
+        # returning false in the callback stops the process
+        return False
 
-def release_callback(key):
-    print('{} released'.format(key))
+    if key == keyboard.Key.f21:
+        print('Toggling lights on/off')
+        if b.get_light(['Hanglamp','Strip','Spotje'], 'on'):
+            b.set_light(['Hanglamp','Strip','Spotje'], 'on', False)
+        
+        if not b.get_light(['Hanglamp','Strip','Spotje'], 'on'):
+            b.set_light(['Hanglamp','Strip','Spotje'], 'on', True)
 
-l = keyboard.Listener(on_press=press_callback,on_release=release_callback)
+    if key == keyboard.Key.f23:
+        print('Lowering brightness')
+        for l in lights:
+            l.brightness -= 1
+
+    if key == keyboard.Key.f22:
+        print('Raising brightness')
+        for l in lights:
+            l.brightness += 1
+
+    if key == keyboard.Key.f15:
+        print('Setting scene 1')
+    
+    if key == keyboard.Key.f16:
+        print('Setting scene 2')
+
+    if key == keyboard.Key.f17:
+        print('Setting scene 3')
+
+    if key == keyboard.Key.f18:
+        print('Setting scene 4')
+
+    if key == keyboard.Key.f19:
+        print('Setting scene 5')
+
+    if key == keyboard.Key.f20:
+        print('Setting scene 6')
+
+l = keyboard.Listener(on_press=press_callback)
 l.start()
 l.join()
